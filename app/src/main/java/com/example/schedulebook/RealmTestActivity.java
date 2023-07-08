1bd0d69ac03f35870f3a9e2d173cad47aa308116
DenimPauel <88633074+DenimPauel@users.noreply.github.com>
2023-07-08 13:35:21 +0900

    89. データベースからデータを取得しよう3～登録・読み取り～

--- app/src/main/java/com/example/schedulebook/RealmTestActivity.java
+++ app/src/main/java/com/example/schedulebook/RealmTestActivity.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Date;
+
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class RealmTestActivity extends AppCompatActivity {
 
@@ -36,6 +39,42 @@ public class RealmTestActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRealm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        Schedule schedule = realm.createObject(Schedule.class, 0);
+                        schedule.setDate( new Date());
+                        schedule.setTitle("登録テスト");
+                        schedule.setDetail("スケジュールの詳細情報です");
+
+                        // 保存するスケジュールをTextViewに表示します。
+                        mTextView.setText("登録しました\n" + schedule.toString());
+                    }
+                });
+            }
+        });
+
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRealm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        RealmResults<Schedule> schedules = realm.where(Schedule.class).findAll();
+                        for (Schedule schedule : schedules){
+                            String text = mTextView.getText() + "\n" + schedule.toString();
+                            mTextView.setText(text);
+                        }
+                    }
+                });
+            }
+        });
+
+
     }
 
     @Override
