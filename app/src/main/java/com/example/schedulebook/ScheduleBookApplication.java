7b5b4cb08156c3e292e6fc3a35f98cd8ea464692
DenimPauel <88633074+DenimPauel@users.noreply.github.com>
2023-07-08 14:19:23 +0900

    90. データベースからデータを取得しよう4～更新・削除～ 　　Realm 10.0以降のUIスレッド上でのトランザクション処理禁止を回避

--- app/src/main/java/com/example/schedulebook/ScheduleBookApplication.java
+++ app/src/main/java/com/example/schedulebook/ScheduleBookApplication.java
@@ -11,5 +11,17 @@ public class ScheduleBookApplication extends Application {
         super.onCreate();
 
         Realm.init(this);
+
+        // FATAL EXCEPTION: main
+        // Process: com.example.schedulebook, PID: 8598
+        // io.realm.exceptions.RealmException: Running transactions on the UI thread has been disabled.
+        // It can be enabled by setting 'RealmConfiguration.Builder.allowWritesOnUiThread(true)'.
+        //【Android】Realm10.0にアップデート後RealmExceptionで落ちてしまう場合の暫定的な対応方法
+        // https://qiita.com/YuukiYoshida/items/62a2203efea97eea2115
+        // Realm 10.0以上では、UIスレッド上での トランザクション処理は禁止になったらしく
+        // UIスレッドで実行させるためには、以下のコードが必要(ただしこれは回避策)
+        RealmConfiguration config = new RealmConfiguration.Builder().allowWritesOnUiThread(true).build();
+        Realm.setDefaultConfiguration(config);
+
     }
 }
