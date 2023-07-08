fdfff62ec27209f880a77c500aa935ce99567bce
DenimPauel <88633074+DenimPauel@users.noreply.github.com>
2023-07-08 13:23:57 +0900

    88. データベースからデータを取得しよう1～Realmの使い方～

--- app/src/main/java/com/example/schedulebook/RealmTestActivity.java
+++ app/src/main/java/com/example/schedulebook/RealmTestActivity.java
@@ -5,20 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import io.realm.Realm;
 
 public class RealmTestActivity extends AppCompatActivity {
+
+    Realm mRealm;
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realm_test);
 
+        mRealm = Realm.getDefaultInstance();
+
+        mTextView   = (TextView) findViewById(R.id.textView);
+        Button create = (Button) findViewById(R.id.create);
+        Button read   = (Button) findViewById(R.id.create);
+        Button update = (Button) findViewById(R.id.update);
+        Button delete = (Button) findViewById(R.id.delete);
+
         Button back = (Button) findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //遷移元に戻る
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mRealm.close();
     }
 }
---
+++ app/src/main/java/com/example/schedulebook/Schedule.java
@@ -0,0 +1,41 @@
+package com.example.schedulebook;
+
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+//データベースのモデルクラス
+public class Schedule extends RealmObject {
+
+    @PrimaryKey
+    public long id;     // 予定を見分けるためのIDが必要
+
+
+    //publicで直接的に書くと、更新できない。Setter Getter があると、回避できた。
+    private Date date;   //予定の日付
+    private String title;    // 予定のタイトル
+    private String detail;   // 予定の詳細
+
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public Date getDate() {
+        return date;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+    public String getDetail() {
+        return detail;
+    }
+}
