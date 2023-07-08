d947bb2d734aee67676e6b83169b15f12924eed4
DenimPauel <88633074+DenimPauel@users.noreply.github.com>
2023-07-08 12:47:08 +0900

    85. 画面遷移を実装してみよう

--- app/src/main/java/com/example/schedulebook/RealmTestActivity.java
+++ app/src/main/java/com/example/schedulebook/RealmTestActivity.java
@@ -3,6 +3,8 @@ package com.example.schedulebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RealmTestActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class RealmTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realm_test);
+
+        Button back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
