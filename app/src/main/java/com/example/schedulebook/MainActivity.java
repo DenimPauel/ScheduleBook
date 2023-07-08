4c30f881eb5b7b64757ce6419663186e4c104a51
DenimPauel <88633074+DenimPauel@users.noreply.github.com>
2023-07-08 23:37:36 +0900

    94. リストビューとアダプタを連携させよう

--- app/src/main/java/com/example/schedulebook/MainActivity.java
+++ app/src/main/java/com/example/schedulebook/MainActivity.java
@@ -6,13 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Realm mRealm;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRealm = Realm.getDefaultInstance();
+        mListView = (ListView) findViewById(R.id.listView);
+
+        //RealmObjectのSchedules を
+        RealmResults<Schedule> schedules = mRealm.where(Schedule.class).findAll();
+        // ScheduleAdapter クラスのインスタンス：adapterを作成
+        ScheduleAdapter adapter = new ScheduleAdapter(schedules);
+        // mListViewにadapterを渡す。場所を指示するだけで、個々のセルの描画は、ここでは気にしない。
+        mListView.setAdapter(adapter);
+
 
         Button dbTest = (Button) findViewById(R.id.db_test_button);
         dbTest.setOnClickListener(new View.OnClickListener() {
