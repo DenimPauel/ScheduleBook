ee7eb1ad4d29fee30fa4bf895df08dbc95f7a817
DenimPauel <88633074+DenimPauel@users.noreply.github.com>
2023-07-09 23:01:36 +0900

    105.ListViewのセルをカスタマイズしてカードビューを使おう2 ※activity_main.xml の listView の divider には、はじめから、@nullを書いてしまっていたが、授業では、105章のカード表示で、ListViwの、セルのラインが見えてしまていたことから、削除するために追加している。

--- app/src/main/java/com/example/schedulebook/ScheduleAdapter.java
+++ app/src/main/java/com/example/schedulebook/ScheduleAdapter.java
@@ -3,6 +3,7 @@ package com.example.schedulebook;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -28,9 +29,17 @@ public class ScheduleAdapter extends RealmBaseAdapter<Schedule> {
         super(data);
     }
 
+    private int[] images = {
+            R.drawable.image0,  //https://pixabay.com/ から画像を収集。
+            R.drawable.image1,
+            R.drawable.image2,
+    };
+
     private static class ViewHolder{
+        ImageView cardImage;
         TextView date;
         TextView title;
+        TextView detail;
     }
 
     //右クリック Generate > Implement Methods >  Select Methods to implement画面から選択
@@ -43,11 +52,14 @@ public class ScheduleAdapter extends RealmBaseAdapter<Schedule> {
 
         if ( convertView == null ){
                         //LayoutInflater クラス xmlからViewを生成する
-            convertView = LayoutInflater.from(parent.getContext())                                 // fromを使ってインスタンスを作成
-                    .inflate(android.R.layout.simple_list_item_2, parent, false);                 // inflateメソッドで、xmlからViewを生成する。
+            convertView = LayoutInflater.from(parent.getContext())                                  // fromを使ってインスタンスを作成
+//                  .inflate(android.R.layout.simple_list_item_2, parent, false);                   // inflateメソッドで、xmlからViewを生成する。
+                    .inflate(R.layout.custom_cell, parent, false);                    // 標準添付のレイアウトから、custom_cell.xmlに変更
             viewHolder = new ViewHolder();                                                          // Viewを束ねたクラス。上で作っている。
-            viewHolder.date  = (TextView) convertView.findViewById(android.R.id.text1);
-            viewHolder.title = (TextView) convertView.findViewById(android.R.id.text2);
+            viewHolder.cardImage = (ImageView) convertView.findViewById(R.id.card_image);
+            viewHolder.date  = (TextView) convertView.findViewById(R.id.date);                      //android.R.id.text1 ->
+            viewHolder.title = (TextView) convertView.findViewById(R.id.title);                     //android.R.id.text2 ->
+            viewHolder.detail = (TextView) convertView.findViewById(R.id.detail);
             convertView.setTag(viewHolder);                                                         //TAGには、プログラマが任意のオブジェクトを設定してよい。
 
         } else {                                                                                    //convertViewが非null(使いまわし)の場合の場合は、
@@ -58,8 +70,10 @@ public class ScheduleAdapter extends RealmBaseAdapter<Schedule> {
                                                                                                     //RealmBaseAdapter.Javaで、protected OrderedRealmCollection<T> adapterData;として定義されている。
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
         String formatDate = sdf.format(schedule.getDate());
-        viewHolder.date.setText(formatDate);                    //ここで表示
+        viewHolder.date.setText(formatDate);
         viewHolder.title.setText(schedule.getTitle());
+        viewHolder.cardImage.setImageResource(images[(int) schedule.getId() % images.length]);
+        viewHolder.detail.setText(schedule.getDetail());
         return convertView;
     }
 }
